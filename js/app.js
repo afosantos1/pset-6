46f1eefd31d401b33abe77b40b4a0fb247a13f4d
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2020-01-05 18:16:47 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-let array = [];
+let empty = []; 
+
 $("ul").on("click", "li", function(event){
 	$(this).toggleClass("completed");
+
 });
+
+
 $("ul").on("click", "span", function(event){
 	if ($(this).hasClass("priority")) {
 		$(this).toggleClass("high-priority");
 		if ($(this).hasClass("high-priority")) {
 			$(this).parent().prependTo("ul");
-			console.log(array);
 		}
 		else {
 			$(this).parent().appendTo("ul");
 		}
+		
 		event.stopPropagation();
+		console.log("high");
 	}
-	else if ($(this).hasClass("trash")) {
-		const id = $(this)[0].id.substring(7);
-		array.splice(id, 1);
+	else {
+		
 		$(this).parent().remove();
 		event.stopPropagation();
 	}
+	
 });
-$("span").click(function(event){
-	console.log("clicked button");
-	input = $("input");
-	inputValue = input.val();
-	if(event.target.id === "button") {
-		if (inputValue === null || inputValue === "") {
-			return;
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		
+		var todoText = $(this).val();
+		$(this).val("");
+		if (todoText != "") {
+			
+			$("ul").append("<li><span class='priority'><i class='fa fa-exclamation-circle'></i></span><span><i class='fa fa-trash'></i></span> " + todoText + " </li>");
 		}
-		array.push({
-			id: array.length,
-			content: inputValue,
-		});
-		const item = array[array.length - 1];
-		$(input).val("");
-		$("ul").append("<li><span class='priority'><i class='fa fa-exclamation-circle'></i></span><span id=delete-" + item.id + " class='trash'><i class='fa fa-trash'></i></span> " + item.content + " </li>");
 	}
 });
