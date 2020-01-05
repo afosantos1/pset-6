b69883c914f1821f0c1413df97db77d2d5b07e5e
Afonso Santos <55449265+afosantos1@users.noreply.github.com>
2020-01-05 15:35:49 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,23 +3,34 @@ var array = [´completed´, ´priority´];
 $("ul").on("click", "li", function(event){
 	$(this).toggleClass("completed");
 });
+
+
 $("ul").on("click", "span", function(event){
 	if ($(this).hasClass("priority")) {
 		$(this).toggleClass("high-priority");
 		if ($(this).hasClass("high-priority")) {
 			$(this).parent().prependTo("ul");
 		}
+		
+		
 		else {
 			$(this).parent().appendTo("ul");
 		}
+		
+		
 		event.stopPropagation();
 		console.log("high");
 	}
+	
+	
 	else {
 		$(this).parent().remove();
 		event.stopPropagation();
 	}	
+	
+	
 });
+[,                                                                             
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var todoText = $(this).val();
@@ -27,5 +38,7 @@ $("input[type='text']").keypress(function(event){
 		if (todoText != "") {
 			$("ul").append("<li><span class='priority'><i class='fa fa-exclamation-circle'></i></span><span><i class='fa fa-trash'></i></span> " + todoText + " </li>");
 		}
+ 
 	}
+ 
 });
